c682f95bf33ae11b6726da956092374e2e7278ef
Carlos-21 <carlos.llontop3@unmsm.edu.pe>
2019-11-25 16:02:54 -0500

    Cambios

--- src/main/kotlin/com/fisi/inventario/configuration/KafkaConfiguration.kt
+++ src/main/kotlin/com/fisi/inventario/configuration/KafkaConfiguration.kt
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
@@ -18,40 +19,52 @@ import com.google.gson.Gson
 
 @Configuration
 @EnableKafka
-class KafkaConfiguration {
+open class KafkaConfiguration {
+	@Value("\${kafka.host:192.168.4.5}")
+    private val host: String? = null
+
+    @Value("\${kafka.port:9092}")
+    private val port: Int = 0
+	
 	@Bean
-	fun producerFactory(): ProducerFactory<String, String>? {
-		val config = HashMap<String?, Any?>()
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
-		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
-		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
-		return DefaultKafkaProducerFactory(config)
-	}
+    open fun producerFactory(): ProducerFactory<String, String> {
+        val configProps = HashMap<String, Any>()
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "$host:$port"
+        configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        return DefaultKafkaProducerFactory(configProps)
+    }
 
 	@Bean
-	fun kafkaTemplate(): KafkaTemplate<String, String>? {
+	open fun kafkaTemplate(): KafkaTemplate<String, String> {
 		return KafkaTemplate(producerFactory())
 	}
 
 	@Bean
-	fun consumerFactory(): ConsumerFactory<String, String>? {
-		val config = HashMap<String?, Any?>()
-		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
-		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
-		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
-		config.put(ConsumerConfig.GROUP_ID_CONFIG, "myGroupId")
-		return DefaultKafkaConsumerFactory(config, StringDeserializer(), StringDeserializer())
-	}
+    open fun consumerConfigs(): Map<String, Any> {
+        val props = HashMap<String, Any>()
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "$host:$port"
+        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        props[ConsumerConfig.GROUP_ID_CONFIG] = "myGroupId2"
 
+        return props
+    }
+
+    @Bean
+    open fun consumerFactory(): ConsumerFactory<String, String> {
+        return DefaultKafkaConsumerFactory(consumerConfigs())
+    }
+	
 	@Bean
-	fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String>? {
+	open fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String>? {
 		val concurrentKafkaListenerContainerFactory: ConcurrentKafkaListenerContainerFactory<String, String> = ConcurrentKafkaListenerContainerFactory()
 		concurrentKafkaListenerContainerFactory.setConsumerFactory(consumerFactory())
 		return concurrentKafkaListenerContainerFactory
 	}
 
 	@Bean
-	fun jsonConverter(): Gson? {
+	open fun jsonConverter(): Gson? {
 		return Gson()
 	}
 }
